d1ac824122ece1596cdb04ee213cdd4fe509ef86
evdudnikov <ev.dudnikov@hotmail.com>
2025-03-02 18:22:09 +0500

    1.Выделены вспомогательные методы. 2. Создан вспомогательный класс для свойств контакта 3. Для создания контакта используются только Имя и Фамилия.

---
+++ AddNewAddressCreationTests.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V133.Storage;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class AddNewAddressCreationTests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "http://localhost/addressbook/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void AddNewAddressCreationTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToNewAddressPage();
+            NewAddressData group = new NewAddressData("Mike", "Adrian");
+            FillAddressForm(group);
+            SubmitNewAddressCreation();
+            ReturnToHomePage();
+            Logout();
+        }
+
+        private void Logout()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+        private void ReturnToHomePage()
+        {
+            driver.FindElement(By.LinkText("home page")).Click();
+        }
+
+        private void SubmitNewAddressCreation()
+        {
+            driver.FindElement(By.XPath("//input[@value='Enter'][2]")).Click();
+        }
+
+        private void FillAddressForm(NewAddressData group)
+        {
+            driver.FindElement(By.Name("firstname")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(group.FirstName);
+            driver.FindElement(By.Name("middlename")).Click();
+            driver.FindElement(By.Name("lastname")).Click();
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(group.LastName);
+            driver.FindElement(By.Name("nickname")).Click();
+            driver.FindElement(By.Name("title")).Click();
+            driver.FindElement(By.Name("company")).Click();
+            driver.FindElement(By.Name("address")).Click();
+            driver.FindElement(By.Name("home")).Click();
+            driver.FindElement(By.Name("mobile")).Click();
+            driver.FindElement(By.Name("work")).Click();
+            driver.FindElement(By.Name("fax")).Click();
+            driver.FindElement(By.Name("email")).Click();
+            driver.FindElement(By.Name("email2")).Click();
+            driver.FindElement(By.Name("email3")).Click();
+            driver.FindElement(By.Name("homepage")).Click();
+            driver.FindElement(By.Name("bday")).Click();
+            driver.FindElement(By.Name("bmonth")).Click();
+            driver.FindElement(By.Name("byear")).Click();
+            driver.FindElement(By.Name("aday")).Click();
+            driver.FindElement(By.Name("amonth")).Click();
+            driver.FindElement(By.Name("ayear")).Click();
+            driver.FindElement(By.Name("new_group")).Click();
+        }
+
+        private void GoToNewAddressPage()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Click();
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Click();
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
---
+++ NewAddressData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class NewAddressData
+    {
+        private string firstname;
+        private string lastname;
+        private string middlename = "";
+        private string nickname = "";
+        private string title = "";
+        private string company = "";
+        private string address = "";
+        private string home = "";
+        private string mobile = "";
+        private string work = "";
+        private string fax = "";
+        private string email = "";
+        private string email2 = "";
+        private string email3 = "";
+        private string homepage = "";
+        private string bday = "";
+        private string bmonth = "";
+        private string byear = "";
+        private string aday = "";
+        private string amonth = "";
+        private string ayear = "";
+        private string new_group = "";
+
+        public NewAddressData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+}
